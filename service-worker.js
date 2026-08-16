198666e4f5c5281f1c3c0fb259c9edf859132f87
usershuffle79 <lifeonshuffle75@gmail.com>
2026-08-16 10:01:59 -0500

    update dasbhoard/fetching

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "budget-v1";
+const CACHE_NAME = "budget-v2";
 
 const FILES_TO_CACHE = [
   "index.html",
@@ -12,6 +12,29 @@ self.addEventListener("install", event => {
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(FILES_TO_CACHE))
   );
+});
+
+
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames
+          .filter(
+            cacheName =>
+              cacheName !== CACHE_NAME
+          )
+          .map(
+            cacheName =>
+              caches.delete(cacheName)
+          )
+      );
+    })
+  );
+
+
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
