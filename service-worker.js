16175a4c40937f7209722fdea4856bf2eb6f3af3
usershuffle79 <lifeonshuffle75@gmail.com>
2026-08-16 16:12:55 -0500

    Upgrade service worker to version 7 and refactor code

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "budget-v6";
+const CACHE_NAME = "budget-v7";
 
 const FILES_TO_CACHE = [
   "index.html",
@@ -7,40 +7,84 @@ const FILES_TO_CACHE = [
   "logotransparent2.png"
 ];
 
+
+/***************************************************************
+ * INSTALL
+ ***************************************************************/
+
 self.addEventListener("install", event => {
+
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(FILES_TO_CACHE))
+    caches
+      .open(CACHE_NAME)
+      .then(cache =>
+        cache.addAll(FILES_TO_CACHE)
+      )
   );
+
+  /*
+   * Activate the new service worker
+   * without waiting for the old one
+   * to disappear.
+   */
+  self.skipWaiting();
 });
 
 
+/***************************************************************
+ * ACTIVATE
+ ***************************************************************/
 
 self.addEventListener("activate", event => {
+
   event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames
-          .filter(
-            cacheName =>
-              cacheName !== CACHE_NAME
-          )
-          .map(
-            cacheName =>
-              caches.delete(cacheName)
-          )
-      );
-    })
+
+    caches
+      .keys()
+      .then(cacheNames => {
+
+        return Promise.all(
+
+          cacheNames
+
+            .filter(
+              cacheName =>
+                cacheName !== CACHE_NAME
+            )
+
+            .map(
+              cacheName =>
+                caches.delete(cacheName)
+            )
+
+        );
+
+      })
+
   );
 
-
+  /*
+   * Allow the newly activated worker
+   * to control open pages immediately.
+   */
   self.clients.claim();
 });
 
+
+/***************************************************************
+ * FETCH
+ ***************************************************************/
+
 self.addEventListener("fetch", event => {
+
   event.respondWith(
-    fetch(event.request).catch(() =>
-      caches.match(event.request)
-    )
+
+    fetch(event.request)
+
+      .catch(() =>
+        caches.match(event.request)
+      )
+
   );
+
 });
